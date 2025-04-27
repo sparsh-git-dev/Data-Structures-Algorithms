ce6307ab2859cd1e5d42e8f63c3a6fffbcc519b9
Sparsh Choudhary <sparsh.choudhary.dev@gmail.com>
2025-04-27 21:09:12 +0530

    sum divisible by k

--- arrays/prefix_sum/Medium/LTC_2845/code.dart
+++ arrays/prefix_sum/Medium/LTC_2845/code.dart
@@ -1,1 +1,14 @@
+// Problem Link:https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+int subarraysDivByK(List<int> nums, int k) {
+  int currSum = 0;
+  int count = 0;
+  Map<int, int> remainderFreq = {0: 1};
+  for (int n in nums) {
+    currSum += n;
+    int needed = (currSum % k + k) % k;
+    count += (remainderFreq[needed] ?? 0);
+    remainderFreq[needed] = (remainderFreq[needed] ?? 0) + 1;
+  }
 
+  return count;
+}
