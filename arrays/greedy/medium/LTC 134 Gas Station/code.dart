75e07925b8d2ab9baa37ef93c7725ca674ab1ab8
Sparsh Choudhary <sparsh.choudhary.dev@gmail.com>
2025-06-24 21:55:09 +0530

    update gas station code

--- arrays/greedy/medium/LTC 134 Gas Station/code.dart
+++ arrays/greedy/medium/LTC 134 Gas Station/code.dart
@@ -1,14 +1,24 @@
 int canCompleteCircuit(List<int> gas, List<int> cost) {
-  int totalTank = 0, currentTank = 0, startIndex = 0;
-  for (int i = 0; i < gas.length; i++) {
-    int gain = gas[i] - cost[i];
-    totalTank += gain;
-    currentTank += gain;
+  int n = gas.length;
 
-    if (currentTank < 0) {
-      startIndex = i + 1;
-      currentTank = 0;
+  int sumGas = 0, sumCost = 0;
+  for (int i = 0; i < gas.length; i++) {
+    sumGas += gas[i];
+    sumCost += cost[i];
+  }
+
+  if (sumGas < sumCost) return -1;
+
+  int total = 0;
+  int result = 0;
+
+  for (int i = 0; i < n; i++) {
+    total += gas[i] - cost[i];
+    if (total < 0) {
+      total = 0;
+      result = i + 1;
     }
   }
-  return totalTank >= 0 ? startIndex : -1;
+
+  return result;
 }
