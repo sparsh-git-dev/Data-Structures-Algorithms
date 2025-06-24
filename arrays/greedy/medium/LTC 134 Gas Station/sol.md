082991fb925dac7835e44a3d759e0bad8b59795a
Sparsh Choudhary <sparsh.choudhary.dev@gmail.com>
2025-06-24 21:59:06 +0530

    add sol

---
+++ arrays/greedy/medium/LTC 134 Gas Station/sol.md
@@ -0,0 +1,1 @@
+![alt text](gasstation.png)
--- coding_ground.dart
+++ coding_ground.dart
@@ -3,30 +3,9 @@ import 'dart:math';
 
 import 'package:collection/collection.dart';
 
-void main() {}
-
-int minimizeMax(List<int> nums, int p) {
-  if (p == 0) return p;
-  nums.sort();
-  int res = nums.last - nums.first;
-  int l = nums.first;
-  int r = res;
-  while (l <= r) {
-    int mid = l + (r - l) ~/ 2;
-    if (isPairPossible(nums, mid, p)) {
-      res = mid;
-      r = mid - 1;
-    } else {
-      l = mid + 1;
-    }
-  }
-  return res;
+void main() {
+  // fun('dabdcbdcdcd');
+  // int sol = maxDistance("EEEWNNNS", 1);
+  // print(sol);
 }
 
-bool isPairPossible(List<int> nums, int target, int pair) {
-  int pairsFound = 0;
-  for (int i = 1; i < nums.length; i++) {
-    if ((nums[i] - nums[i - 1]).abs() <= target) pairsFound++;
-  }
-  return pairsFound == pair;
-}
