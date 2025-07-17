3d36a8581c07e3aef7dc0f20d040e34f3ea94cd1
Sparsh Choudhary <sparsh.choudhary.dev@gmail.com>
2025-07-17 21:16:10 +0530

    dp shift by 1 index

--- dynamic_programming/medium/ltc 3201 find the maxm length of valid subseq 1/code.dart
+++ dynamic_programming/medium/ltc 3201 find the maxm length of valid subseq 1/code.dart
@@ -1,20 +1,29 @@
 import 'dart:math';
 
 class Solution {
-  int _lis(List<int> nums, int index, int prevIndex, int mod) {
+  int _lis(
+      List<int> nums, int index, int prevIndex, int mod, List<List<int>> dp) {
     if (index >= nums.length) return 0;
+    int dpPrevShift = prevIndex + 1;
+    if (dp[dpPrevShift][index] != -1) return dp[dpPrevShift][index];
 
     int take = 0;
     if (prevIndex == -1 || (nums[prevIndex] + nums[index]) % 2 == mod) {
-      take = 1 + _lis(nums, index + 1, index, mod);
+      take = 1 + _lis(nums, index + 1, index, mod, dp);
     }
 
-    int skip = _lis(nums, index + 1, prevIndex, mod);
+    int skip = _lis(nums, index + 1, prevIndex, mod, dp);
 
-    return max(take, skip);
+    return dp[dpPrevShift][index] = max(take, skip);
   }
 
   int maximumLength(List<int> nums) {
-    return max(_lis(nums, 0, -1, 0), _lis(nums, 0, -1, 1));
+    int n = nums.length;
+    List<List<int>> dp1 =
+        List.generate(n + 1, (index) => List.filled(n + 1, -1));
+    List<List<int>> dp2 =
+        List.generate(n + 1, (index) => List.filled(n + 1, -1));
+
+    return max(_lis(nums, 0, -1, 0, dp1), _lis(nums, 0, -1, 1, dp2));
   }
 }
