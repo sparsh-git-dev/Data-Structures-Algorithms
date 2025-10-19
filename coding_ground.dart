b698980a7685ee33e4605df03aa086acd9f212c4
Sparsh Choudhary <sparsh.choudhary.dev@gmail.com>
2025-10-19 17:12:26 +0530

    aadd code

--- coding_ground.dart
+++ coding_ground.dart
@@ -1,4 +1,30 @@
 import 'dart:math';
 
-void main() {}
+void main() {
+  maxDistinctElements([8, 8, 10, 9, 9], 1);
+}
 
+int maxDistinctElements(List<int> nums, int k) {
+  nums.sort();
+  List<int> req = [];
+  Set<int> unique = {};
+  for (int n in nums) {
+    if (unique.contains(n)) {
+      req.add(n);
+    } else {
+      unique.add(n);
+    }
+  }
+
+  for (int r in req) {
+    for (int j = -k; j <= k; j++) {
+      int num = r + j;
+      if (unique.contains(num) == false) {
+        unique.add(num);
+        break;
+      }
+    }
+  }
+
+  return unique.length;
+}
---
+++ maths/LTC 3397 Maximum Number of Distinct Elements After Operations/code.dart
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/description/
+class Solution {
+  int maxDistinctElements(List<int> nums, int k) {
+    int n = nums.length;
+    nums.sort();
+    int count = 0;
+    int prev = -1e9.toInt();
+
+    for (int i = 0; i < n; i++) {
+      int minVal = nums[i] - k;
+
+      if (prev < minVal) {
+        // Choose nums[i] - k
+        prev = minVal;
+        count++;
+      } else if (prev  + 1 <= nums[i] + k) {
+        // Choose next available distinct value greater than prev
+        prev = prev + 1;
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
