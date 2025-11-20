945fed5e77a35f776c3a2aa33dbf0b90d2f9722a
Sparsh Choudhary <sparsh.choudhary.dev@gmail.com>
2025-11-20 20:59:56 +0530

    add floyd warhal problem solution

--- coding_ground.dart
+++ coding_ground.dart
@@ -2,26 +2,33 @@ import 'dart:math';
 
 import 'package:collection/collection.dart';
 
-void main() {
-  final a = [3, 1, 2, 1];
-  final b = [0, 2];
-  int ans = minOperations(b);
-  print(ans);
-}
+void main() {}
 
-int minOperations(List<int> nums) {
-  PriorityQueue queue = PriorityQueue();
-  if (nums.first != 0) queue.add(nums.first);
-  int ans = 0;
-  for (int i = 1; i < nums.length; i++) {
-    int value = nums[i];
-    while (queue.isNotEmpty && queue.first > value) {
-      queue.removeFirst();
-      ans++;
-    }
-    if (queue.isEmpty && value != 0 ||
-        (queue.isNotEmpty && queue.contains(value))) queue.add(value);
+int intersectionSizeTwo(List<List<int>> intervals) {
+  intervals.sort((x, y) {
+    int firstStart = x[0], firstEnd = x[1];
+    int secondStart = y[0], secondEnd = y[1];
+    return (firstEnd == secondEnd) ? (secondStart - firstStart) : (firstEnd - secondEnd);
+  });
+
+  int count = 2;
+  int n = intervals.length;
+
+  int b = intervals[0][1];
+  int a = b - 1;
+
+  for (int i = 1; i < n; i++) {
+    int l = intervals[i][0];
+    int r = intervals[i][1];
+
+    if (a >= l) continue;
+
+    bool flag = l > b;
+    count += 1 + (flag ? 1 : 0);
+
+    a = flag ? r - 1 : b;
+    b = r;
   }
-  ans += queue.toSet().length;
-  return ans;
+
+  return count;
 }
---
+++ graphs/algo/floyd warshall/Problem/medium/LTC 2976. Minimum Cost to Convert String I/code.dart
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/minimum-cost-to-convert-string-i/
+
+import 'dart:math';
+
+int minimumCost(String source, String target, List<String> original,
+    List<String> changed, List<int> cost) {
+  int INF = 1e9.toInt();
+  List<List<int>> dis = List.generate(26, (_) => List.filled(26, INF));
+
+  for (int i = 0; i < 26; i++) dis[i][i] = 0;
+  int aCode = 'a'.codeUnitAt(0);
+
+  for (int i = 0; i < cost.length; i++) {
+    int u = original[i].codeUnitAt(0) - aCode;
+    int v = changed[i].codeUnitAt(0) - aCode;
+    dis[u][v] = min(dis[u][v], cost[i]);
+  }
+
+  for (int k = 0; k < 26; k++) {
+    for (int i = 0; i < 26; i++) {
+      if (dis[i][k] == INF) continue;
+      for (int j = 0; j < 26; j++) {
+        if (dis[k][j] == INF) continue;
+        dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+      }
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < source.length; i++) {
+    if (source[i] == target[i]) continue;
+    int u = source[i].codeUnitAt(0) - aCode;
+    int v = target[i].codeUnitAt(0) - aCode;
+    if (dis[u][v] == INF) return -1;
+    ans += dis[u][v];
+  }
+  return ans;
+}
