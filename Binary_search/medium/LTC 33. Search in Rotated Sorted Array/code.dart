5785c1aa63f7b499bfd785ebc1dac62fb1aad393
sparsh choudhary <sparsh.choudhary@simplifyvms.com>
2026-05-22 18:57:35 +0530

    binary search on rotated array

---
+++ Binary_search/medium/LTC 33. Search in Rotated Sorted Array/code.dart
@@ -0,0 +1,27 @@
+int search(List<int> nums, int target) {
+  int s = 0, e = nums.length - 1;
+  while (s <= e) {
+    int m = (s + e) ~/ 2;
+    if (nums[m] == target) return m;
+    // Edge case: eg [3,1]
+    /// s =0, m =0,
+    /// we are giving guranateed that left side is always sorted thats why
+    /// <= not just <
+    /// 
+    if (nums[s] <= nums[m]) { 
+      if (nums[s] <= target && target < nums[m]) {
+        e = m - 1;
+      } else {
+        s = m + 1;
+      }
+    } else {
+      if (nums[m] < target && target <= nums[e]) {
+        s = m + 1;
+      } else {
+        e = m - 1;
+      }
+    }
+  }
+
+  return -1;
+}
--- coding_ground.dart
+++ coding_ground.dart
@@ -42,3 +42,27 @@ int minimumPairRemoval(List<int> nums) {
 
   return ans;
 }
+
+int search(List<int> nums, int target) {
+  int s = 0, e = nums.length - 1;
+
+  while (s <= e) {
+    int m = (s + e) ~/ 2;
+    if (nums[m] == target) return m;
+    if (nums[s] < nums[m]) {
+      if (nums[s] < target && target < nums[m]) {
+        e = m - 1;
+      } else {
+        s = m + 1;
+      }
+    } else {
+      if (nums[m] < target && target < nums[e]) {
+        s = m + 1;
+      } else {
+        e = m - 1;
+      }
+    }
+  }
+
+  return -1;
+}
