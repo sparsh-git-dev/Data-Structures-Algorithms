39342005dbd719a9b0f38b23f7e451d11d0bdfad
sparsh choudhary <sparsh.choudhary@simplifyvms.com>
2026-06-03 16:12:45 +0530

    code commenting on the greedy sol

--- greedy/medium/LTC 3635. Earliest Finish Time for Land and Water Rides/code.dart
+++ greedy/medium/LTC 3635. Earliest Finish Time for Land and Water Rides/code.dart
@@ -14,12 +14,19 @@ class Solution {
   int bestFinishTime(List<int> firstStart, List<int> firstDuration,
       List<int> secondStart, List<int> secondDuration) {
     int earliestFirstFinish = 1e9.toInt();
+    // Finding the earliest ending ride
     for (int i = 0; i < firstStart.length; i++) {
       earliestFirstFinish =
           min(earliestFirstFinish, firstStart[i] + firstDuration[i]);
     }
 
     int earliestBothFinish = 1e9.toInt();
+    // We need to add the duration no matter what,
+    // first ride ended at 5, for the second ride we have option [3,7]
+    // then in the ans for i =0, => 3, it occured earlier hence the max 7 is chosen,
+    // for i =1, => 7, we need to wait for 2 hours , hence max(5,7) is 7,
+    // either we chose i =0 or 1, we still have to add the duration of
+    // that particular ride
     for (int i = 0; i < secondStart.length; i++) {
       earliestBothFinish = min(
         earliestBothFinish,
